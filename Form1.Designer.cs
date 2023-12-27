4e462848dacb71dcad62f0295fe99126d5b3b1f2
Vadim Voitsehovskiy <vadim.rolex.2005@gmail.com>
2023-12-27 19:09:13 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace lr_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(-3, 465);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 35);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(680, 498);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace lr_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.FillEllipse(Brushes.White, 150, 50, 100, 100);
+            g.FillEllipse(Brushes.White, 140, 140, 120, 120);
+            g.FillEllipse(Brushes.White, 133, 240, 140, 140);
+
+            Point[] nose = {
+                new Point(190, 100), 
+                new Point(210, 100),
+                new Point(200, 130)
+            };
+
+            g.FillPolygon(Brushes.Orange, nose);
+            g.FillEllipse(Brushes.Black, 210, 80, 10, 10);
+            g.FillEllipse(Brushes.Black, 180, 80, 10, 10);
+
+            Point[] hat = {
+                new Point(175, 15),
+                new Point(155, 75),
+                new Point(245, 75),
+                new Point(225, 15)
+            };
+
+            g.FillPolygon(Brushes.Red, hat);
+
+            Pen pen = new Pen(Color.Black, 1);
+            g.DrawPolygon(pen, hat);
+            g.DrawEllipse(Pens.Black, 197, 180, 10, 10);
+            g.DrawEllipse(Pens.Black, 197, 230, 10, 10);
+            g.DrawEllipse(Pens.Black, 197, 280, 10, 10);
+
+            Point[] br = {
+                new Point(320, 100),
+                new Point(320, 370)
+            };
+
+            Pen pen1 = new Pen(Color.Black, 5);
+
+            g.DrawPolygon(pen1, br);
+
+            Point[] h1 = {
+                new Point(260, 200),
+                new Point(320, 150)
+            };
+
+            Pen pen2 = new Pen(Color.Black, 5);
+
+            g.DrawPolygon(pen2, h1);
+
+            Point[] h2 = {
+                new Point(80, 150),
+                new Point(140, 200)
+            };
+
+            Pen pen3 = new Pen(Color.Black, 5);
+
+            g.DrawPolygon(pen3, h2);
+
+            Random random = new Random();
+
+            for (int i = 0; i < 50; i++)
+            {
+                int x = random.Next(0, 500);
+                int y = random.Next(0, 400);
+                g.FillEllipse(Brushes.White, x, y, 10, 10);
+            }
+        }
+    }
+}
